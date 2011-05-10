e8faaa0c6473e454c9778f60d6475c219f1e2bde
Postmodern <postmodern.mod3@gmail.com>
2011-05-10 16:02:47 -0700

    Add a message for why CRC5 specs are pending.

--- spec/crc5_spec.rb
+++ spec/crc5_spec.rb
@@ -3,12 +3,13 @@ require 'crc_examples'
 require 'digest/crc5'
 
 describe Digest::CRC5 do
-  before { pending }
   before(:all) do
     @crc_class = Digest::CRC5
     @string = '1234567890'
     @expected = '1'
   end
 
-  it_should_behave_like "CRC"
+  pending "Implementation of CRC5 does not match pycrc.py" do
+    it_should_behave_like "CRC"
+  end
 end
