98d5b21f32af148a6dfe17d06a4e39be35f2cbf3
Postmodern <postmodern.mod3@gmail.com>
2012-10-08 19:25:03 -0700

    Style.

--- lib/digest/crc16_qt.rb
+++ lib/digest/crc16_qt.rb
@@ -7,8 +7,11 @@ module Digest
   # @author Matthew Bednarski
   #
   class CRC16QT < CRC16CCITT
+
     FINAL_XOR = 0xffff
+
     REVERSE_CRC_RESULT = true
+
     REVERSE_DATA = true
 
     #
@@ -21,7 +24,7 @@ module Digest
     #   The packed CRC16 checksum.
     #
     def self.pack(crc)
-      crc ^= FINAL_XOR if FINAL_XOR
+      crc ^= FINAL_XOR      if FINAL_XOR
       crc = revert_bits crc if REVERSE_CRC_RESULT
 
       buffer = ''
@@ -40,33 +43,37 @@ module Digest
     #
     def update(data)
       data.each_byte do |b|
-        b = revert_byte b if REVERSE_DATA
+        b = revert_byte(b) if REVERSE_DATA
         @crc = ((TABLE[((@crc >> 8) ^ b) & 0xff] ^ (@crc << 8)) & 0xffff)
       end
 
       return self
     end
 
-    private
+    protected
+
     def self.revert_bits(cc)
       ob = 0
-      b = (1 << 15)
+      b  = (1 << 15)
+
       16.times do |t|
         ob |= (1 << t) if (cc & b) != 0
         b >>= 1
       end
-      ob
+
+      return ob
     end
 
     def revert_byte(cc)
       ob = 0
-      b = (1 << 7)
+      b  = (1 << 7)
+
       8.times do |t|
         ob |= (1 << t) if (cc & b) != 0
         b >>= 1
       end
-      ob
+
+      return ob
     end
   end
 end
-
