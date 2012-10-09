3970f79a9cf65f6e18b0ea03c245e3896c540ecf
Postmodern <postmodern.mod3@gmail.com>
2012-10-08 19:26:09 -0700

    Fixed the CRC16QT spec.

--- spec/crc16_qt_spec.rb
+++ spec/crc16_qt_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 require 'crc_examples'
-require 'digest/crc16_ccitt'
+require 'digest/crc16_qt'
 
-describe Digest::CRC16CCITT do
+describe Digest::CRC16QT do
   let(:string)   { '1234567890' }
   let(:expected) { '4b13' }
 
