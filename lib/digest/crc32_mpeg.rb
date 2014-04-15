0e53fa219148d01fa770250d1f384ff4c3d98e2a
Ivan <ivankorunkov@ya.ru>
2014-04-15 11:47:33 +0400

    Defend the TABLEs from runtime changes. :snowflake: Freeze this constants.

--- lib/digest/crc32_mpeg.rb
+++ lib/digest/crc32_mpeg.rb
@@ -73,7 +73,7 @@ module Digest
       0x9abc8bd5, 0x9e7d9662, 0x933eb0bb, 0x97ffad0c,
       0xafb010b1, 0xab710d06, 0xa6322bdf, 0xa2f33668,
       0xbcb4666d, 0xb8757bda, 0xb5365d03, 0xb1f740b4
-    ]
+    ].freeze
 
     #
     # Updates the CRC32 Mpeg checksum.
