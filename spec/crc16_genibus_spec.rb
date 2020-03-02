cde479cb625498fda358561066f58b9101aa17a8
Postmodern <postmodern.mod3@gmail.com>
2020-03-02 02:49:15 -0800

    Fixed the initial and final XOR masks for Digest::CRC16Genibus.

--- spec/crc16_genibus_spec.rb
+++ spec/crc16_genibus_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 require 'crc_examples'
-require 'digest/crc16_modbus'
+require 'digest/crc16_genibus'
 
-describe Digest::CRC16Modbus do
+describe Digest::CRC16Genibus do
   let(:string)   { '1234567890' }
-  let(:expected) { 'c20a' }
+  let(:expected) { 'cde7' }
 
   it_should_behave_like "CRC"
 end
