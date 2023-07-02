575a2dad5962db4f9f64608494183b84cefb8922
dearblue <dearblue@users.osdn.me>
2023-07-02 18:53:06 +0900

    Fixed `Digest::CRC5`
    Previous implementations had the following problems:
    
      - The internal value was always calculated to be shifted three bits to the left.
      - The external output should have been shifted three bits to the right, but was not.
      - The C implementation was not bit-shifted.
    
    As a result, the correct calculation was not being done.
    
    With this patch, the Ruby implementation is now also not left bit-shifted, and the calculation is no longer a problem.
    
    However, there are the following compatibility concerns:
    
      - When users have made their own modifications
      - When users refer to `Digest::CRC5::TABLE`.

--- spec/crc5_spec.rb
+++ spec/crc5_spec.rb
@@ -4,9 +4,7 @@ require 'digest/crc5'
 
 describe Digest::CRC5 do
   let(:string)   { '1234567890' }
-  let(:expected) { '1' }
+  let(:expected) { '01' }
 
-  pending "Implementation of CRC5 does not match pycrc.py" do
-    it_should_behave_like "CRC"
-  end
+  it_should_behave_like "CRC"
 end
